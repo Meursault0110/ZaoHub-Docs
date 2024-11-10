9a932d560fda9b7e9d8b6e35c5de6a36b7c6015f
TTK-095 <nothingmatters479@proton.me>
2024-11-10 11:10:59 +0100

    Añadiendo contenido (Index, Cheatsheets, etc.)

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,32 +22,83 @@ export default defineConfig({
 						{
 							label: "ISO",
 							collapsed: true,
-							items: [],
+							items: [
+								{label: "1. Intro to OSs", link: "asir-notes/iso/os-intro"},
+								{label: "2. Intro to Virtualization", link: "asir-notes/iso/vm-intro"},
+							],
 						},
 						{
 							label: "FHW",
 							collapsed: true,
-							items: [],
-						},
-						{
-							label: "OSs",
-							collapsed: true,
-							items: [],
-						},
-						{
-							label: "Bases de Datos",
-							collapsed: true,
-							items: [],
-						},
-						{
-							label: "Redes",
-							collapsed: true,
-							items: [],
+							items: [
+								{label: "1. Numeritos", link: "#"},
+								{label: "2. Computer Architecture", link: "asir-notes/fhw/computer-arch"},
+							],
 						},
 						{
 							label: "Lenguajes de Marcas",
 							collapsed: true,
 							items: [],
+						},
+					],
+				},
+				{
+					label: "📒 Cheatsheets",
+					collapsed: true,
+					items: [
+						{label: "🔶 Git", link: "cheatsheets/git-cs"},
+						{label: "🔐 ssh", link: "cheatsheets/ssh-cs"},
+						{label: "🔏 scp", link: "#"},
+						{label: "🐳 Docker", link: "#"},
+						{label: "🦈 Wireshark/Tshark", link: "#"},
+						{
+							label: "CMS Tools",
+							collapsed: true,
+							items: [
+								{label: "WPscan (WordPress)", link: "#"},
+								{label: "Joomscan (Joomla)", link: "#"},
+								{label: "Droopescan (Drupal)", link: "#"},
+								{label: "Magescan (Magento)", link: "#"},
+							],
+						},
+						{
+							label: "DataBases",
+							collapsed: true,
+							items: [
+								{label: "SQL", link: "#"},
+								{label: "MySQL", link: "#"},
+								{label: "PostgreSQL", link: "#"},
+								{label: "NOSQL", link: "#"},
+							],
+						},
+						{
+							label: "Linux-Tools",
+							collapsed: true,
+							items: [
+								{label: "find", link: "#"},
+								{label: "awk", link: "#"},
+								{label: "grep", link: "#"},
+								{label: "sed", link: "#"},
+							],
+						},
+						{
+							label: "Pentesting",
+							collapsed: true,
+							items: [
+								{label: "sqlmap", link: "#"},
+								{label: "Metasploit", link: "#"},
+								{label: "aircrack-ng", link: "#"},
+								{label: "nmap", link: "#"},
+							],
+						},
+						{
+							label: "Fuzzing",
+							collapsed: true,
+							items: [
+								{label: "Wfuzz", link: "#"},
+								{label: "Gobuster", link: "#"},
+								{label: "fuff", link: "#"},
+							],
 						},
 					],
 				},
@@ -60,7 +111,7 @@ export default defineConfig({
 							collapsed: true,
 							items: [
 								{
-									label: "🧱 Architecture",
+									label: "🧱 COA",
 									collapsed: true,
 									items: [],
 								},
@@ -77,16 +128,8 @@ export default defineConfig({
 											label: "🐧 Linux",
 											collapsed: true,
 											items: [
-												{
-													label: "⚙ System",
-													collapsed: true,
-													items: [],
-												},
-												{
-													label: "🈺 Scripting",
-													collapsed: true,
-													items: [],
-												},
+												{label: "⚙ System", link: "it-fundamentals/it-essentials/os/linux/system"},
+												{label: "🈺 Scripting", link: "it-essentials/os/linux/scripting"},
 											],
 										},
 										{
@@ -118,6 +161,37 @@ export default defineConfig({
 					],
 				},
 				{
+					label: "🛢 DataBases", 
+					collapsed: true,
+					items: [
+						{
+							label: "Introduction to DBs", link: "#"
+						},
+						{
+							label: "Design DBs", link: "#"
+						},
+						{
+							label: "Programming DBs", link: "#"
+
+						},
+						{
+							label: "Data Analytics (Python)", link: "#"
+
+						},
+					],
+				},
+				{
+					label: "🐧 Linux Hardening", 
+					collapsed: true,
+					items: [],
+				},
+				{
+					label: "⏹ Windows Hardening", 
+					collapsed: true,
+					items: [],
+				},
+
+				{
 					label: "⛩ Programación",
 					collapsed: true,
 					items: [
@@ -141,11 +215,21 @@ export default defineConfig({
 									items: [
 										{
 											label: "Git",
-											items: [],
+											items: [
+												{label: "Introducción", link: "#"},
+												{label: "Configuración Inicial", link: "#"},
+												{label: "Básico", link: "#"},
+												{label: "Avanzado", link: "#"},
+											],
 										},
 										{
 											label: "GitHub",
-											items: [],
+											items: [
+												{label: "Introducción", link: "#"},
+												{label: "Creación y Configuración de repositorio", link: "#"},
+												{label: "Básico", link: "#"},
+												{label: "Avanzado", link: "#"},
+											],
 										},
 									],
 								},
@@ -155,11 +239,7 @@ export default defineConfig({
 							label: "🎲 Lenguajes",
 							collapsed: true,
 							items: [
-								{
-									label: "Python",
-									collapsed: true,
-									items: [],
-								},
+								{label: "Python", link: "programming/programming-languages/python/python-index"},
 								{
 									label: "C",
 									collapsed: true,
@@ -196,13 +276,65 @@ export default defineConfig({
 					],
 				},
 				{
+					label: "👥 OSINT",
+					collapsed: true,
+					items: [
+						{label: "0. Introduction", link: "#"},
+						{label: "1. Search Engines", link: "#"},
+						{label: "2. Specializad Tools", link: "#"},
+						{label: "3. Social Avenues", link: "#"},
+						{label: "4. Data Breaches", link: "#"},
+						{label: "5. Dark Web", link: "#"},
+					],
+				},
+				{
+					label: "🎭 Social Engineering",
+					collapsed: true,
+					items: [
+						{label: "0. Introduction", link: "#"},
+						{label: "1. OSINT", link: "#"},
+						{label: "2. Profiling/DISC", link: "#"},
+						{label: "3. Pretexting", link: "#"},
+						{label: "4. Robin Dreeke", link: "#"},
+						{label: "5. Robert Cialdini", link: "#"},
+						{label: "6. ", link: "#"},
+						{label: "7. ", link: "#"},
+						{label: "8. ", link: "#"},
+						{label: "9. ", link: "#"},
+						{label: "10. ", link: "#"},
+					],
+				},
+				{
+					label: "🌀 General Pentesting",
+					collapsed: true,
+					items: [
+						{label: "Pentesting Methodology", link: "#"},
+						{label: "Enumeration Fundamentals", link: "#"},
+						{label: "Explotation Fundamentals", link: "#"},
+					],
+				},
+				{
 					label: "⚙ OSs Pentesting",
 					collapsed: true,
 					items: [
 						{
 							label: "Linux Privesc",
 							collapsed: true,
-							items: [],
+							items: [
+								{ label: "Abusing Sudoers Privileges", link: "#" },
+								{ label: "Abusing SUID Privileges", link: "#" },
+								{ label: "Detection and exploitation of Cron tasks", link: "#" },
+								{ label: "PATH Hijacking", link: "#" },
+								{ label: "Python Library Hijacking", link: "#" },
+								{ label: "Abuse of incorrectly implemented permissions", link: "#" },
+								{ label: "Detection and exploitation of Capabilities", link: "#" },
+								{ label: "Kernel Exploitation", link: "#" },
+								{ label: "Abuse of special user groups", link: "#" },
+								{ label: "Abuse of internal system services", link: "#" },
+								{ label: "Abuse of specific binaries", link: "#" },
+								{ label: "Hijacking the library of dynamically linked shared objects", link: "#" },
+								{ label: "Docker Breakout", link: "#" },
+							],
 						},
 						{
 							label: "Windows Privesc",
@@ -229,17 +361,50 @@ export default defineConfig({
 						{
 							label: "Web Vulns",
 							collapsed: true,
-							items: [],
+							items: [
+								{ label: "Cross-site-Scripting", link: "#" },
+								{ label: "Local File Inclusion", link: "#" },
+								{ label: "Remote File Inclusion", link: "#" },
+								{ label: "Log Poisoning", link: "#" },
+								{ label: "Cross Site Request Forgery", link: "#" },
+								{ label: "Server Side Request Forgery", link: "#" },
+								{ label: "Server Side Template Injection", link: "#" },
+								{ label: "Client Side Server Injection", link: "#" },
+								{ label: "Type Jugling Attack", link: "#" },
+								{ label: "XML External Entity Injection", link: "#" },
+								{ label: "SQL Injection", link: "#" },
+								{ label: "NOSQL Injection", link: "#" },
+								{ label: "Header Injection", link: "#" },
+								{ label: "LDAP Injection", link: "#" },
+								{ label: "LaTeX Injection", link: "#" },
+								{ label: "XPath Injection", link: "#" },
+								{ label: "File Uploads", link: "#" },
+								{ label: "Padding Oracle Attack", link: "#" },
+								{ label: "Deserialization Attacks", link: "#" },
+								{ label: "Prototype Pollution", link: "#" },
+								{ label: "Full Zone Transfer Attack", link: "#" },
+								{ label: "Mass Assignment Attack", link: "#" },
+								{ label: "Open Redirect", link: "#" },
+								{ label: "ShellShock Attack", link: "#" },
+								{ label: "IDOR", link: "#" },
+								{ label: "CORS", link: "#" },
+								{ label: "SQL Truncation", link: "#" },
+								{ label: "Session Puzzling", link: "#" },
+								{ label: "Race Condition", link: "#" },
+								{ label: "CSS Injection", link: "#" },
+							],
 						},
 						{
-							label: "Networking Pentesting",
+							label: "API Pentesting",
 							collapsed: true,
 							items: [],
 						},
 						{
 							label: "Web Tech Pentesting",
 							collapsed: true,
-							items: [],
+							items: [
+								
+							],
 						},
 					],
 				},
@@ -248,17 +413,17 @@ export default defineConfig({
 					collapsed: true,
 					items: [
 						{
-							label: "Web Vulns",
+							label: "Wifi",
 							collapsed: true,
 							items: [],
 						},
 						{
-							label: "Networking Pentesting",
+							label: "Bluetooth",
 							collapsed: true,
 							items: [],
 						},
 						{
-							label: "Web Tech Pentesting",
+							label: "SDR",
 							collapsed: true,
 							items: [],
 						},
@@ -268,12 +433,27 @@ export default defineConfig({
 					label: "📱 Mobile Pentesting",
 					collapsed: true,
 					items: [
+						{
+							label: "🟢 Android",
+							collapsed: true,
+							items: [],
+						},
+						{
+							label: " 🔵 IOS",
+							collapsed: true,
+							items: [],
+						},
 					],
 				},
 				{
 					label: "🎱 Low Level Pentesting",
 					collapsed: true,
 					items: [
+						{
+							label: "Buffer Overflow",
+							collapsed: true,
+							items: [],
+						},
 						{
 							label: "Binary Exploitation",
 							collapsed: true,
@@ -313,15 +493,14 @@ export default defineConfig({
 					],
 				},
 				{
-					label: "👥 OSINT",
+					label: "🔷 Blue Team",
 					collapsed: true,
 					items: [
-					],
-				},
-				{
-					label: "🎭 Social Engineering",
-					collapsed: true,
-					items: [
+						{
+							label: "Malware Analysis",
+							collapsed: true,
+							items: [],
+						},
 					],
 				},
 				{
@@ -375,7 +554,7 @@ export default defineConfig({
 									items: [],
 								},
 								{
-									label: "PHP && MYSQL",
+									label: "PHP",
 									collapsed: true,
 									items: [],
 								},
@@ -385,6 +564,22 @@ export default defineConfig({
 									items: [],
 								},
 							],
+						},
+					],
+				},
+				{
+					label: "🎩 English",
+					collapsed: true,
+					items: [
+						{
+							label: "English for IT",
+							collapsed: true,
+							items: [],
+						},
+						{
+							label: "English (General)",
+							collapsed: true,
+							items: [],
 						},
 					],
 				},
